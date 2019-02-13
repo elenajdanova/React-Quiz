6eb7e2c67a62dbe2c3b224b218f8d9245d0055c3
Elena <litishia@ya.ru>
2019-02-13 16:45:44 -0500

    refactored answer display

--- src/components/AnswersDisplay.js
+++ src/components/AnswersDisplay.js
@@ -1,28 +1,19 @@
 import React from 'react';
 
 
-const AnswersDisplay = (props) => {
-
-  const options = () => {
-    for (let i=0; i < 4; i+2) {
-    
-    return (
-      <div>
-      <div className="ui two buttons hSpace">
-        <button className="ui basic green button">{props.answerList[i]}</button>
-        <button className="ui basic green button">{props.answerList[i+1]}</button>
-      </div>
-      <div className="ui two buttons hSpace">
-        <button className="ui basic green button">{props.answerList[i+2]}</button>
-        <button className="ui basic green button">{props.answerList[i+3]}</button>
-      </div>
-      </div>
-    )
-    }
-  };
+const AnswersDisplay = (props) => {  
 
   return(
-    <div> {options()} </div>
+    <div>
+    <div className="ui two buttons hSpace">
+      <button className="ui basic green button">{props.answerList[0]}</button>
+      <button className="ui basic green button">{props.answerList[1]}</button>
+    </div>
+    <div className="ui two buttons hSpace">
+      <button className="ui basic green button">{props.answerList[2]}</button>
+      <button className="ui basic green button">{props.answerList[3]}</button>
+    </div>
+    </div>
   );
 
 };
