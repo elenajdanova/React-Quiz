9fb43cafb550904410bc41d52af7b2dfc0d1ca3f
Elena <litishia@ya.ru>
2019-02-13 17:55:23 -0500

    added check correct answer logic

--- src/components/AnswersDisplay.js
+++ src/components/AnswersDisplay.js
@@ -1,17 +1,35 @@
 import React from 'react';
 
 
-const AnswersDisplay = (props) => {  
+const AnswersDisplay = (props) => {
+
+  const onAnswer = (e) =>{
+    let userAnswer = e.target.value;
+    if (userAnswer === props.correctAnswer) {
+      //call callback from the index component (true)
+    } else {
+      //call callback from the index component (false)
+    }
+    //console.log(userAnswer + props.correctAnswer);
+  }
 
   return(
     <div>
     <div className="ui two buttons hSpace">
-      <button className="ui basic green button">{props.answerList[0]}</button>
-      <button className="ui basic green button">{props.answerList[1]}</button>
+      <button onClick={onAnswer}
+              value={props.answerList[0]}
+              className="ui basic green button">{props.answerList[0]}</button>
+      <button onClick={onAnswer}
+              value={props.answerList[1]}
+              className="ui basic green button">{props.answerList[1]}</button>
     </div>
     <div className="ui two buttons hSpace">
-      <button className="ui basic green button">{props.answerList[2]}</button>
-      <button className="ui basic green button">{props.answerList[3]}</button>
+      <button onClick={onAnswer}
+              value={props.answerList[2]}
+              className="ui basic green button">{props.answerList[2]}</button>
+      <button onClick={onAnswer}
+              value={props.answerList[3]}
+              className="ui basic green button">{props.answerList[3]}</button>
     </div>
     </div>
   );
