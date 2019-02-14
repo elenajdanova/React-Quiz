04b5486bde9663e80a2fa7b77e67cd70b10ec15f
Elena <litishia@ya.ru>
2019-02-14 12:59:11 -0500

    refactored answer render

--- src/components/AnswersDisplay.js
+++ src/components/AnswersDisplay.js
@@ -2,35 +2,42 @@ import React from 'react';
 
 
 const AnswersDisplay = (props) => {
+  const answerOptions = props.answerList.map( (option) => {
+    return (
+      <button
+        onClick={onAnswer}
+        value={option}
+        className="ui basic green button">{option}
+      </button>
+    )
+  });
+
+  let flag = false;
+  const renderAnswers = () => {
+    let list = answerOptions;
+      if (flag === false) {
+        flag = true;
+        return list.slice(0,2)
+      } else {
+        return list.slice(2,4)
+      }
+
+  };
 
   const onAnswer = (e) =>{
     let userAnswer = e.target.value;
-    if (userAnswer === props.correctAnswer) {
-      //call callback from the index component (true)
-    } else {
-      //call callback from the index component (false)
-    }
-    //console.log(userAnswer + props.correctAnswer);
+    // if (userAnswer === props.correctAnswer) {
+    //   //call callback from the index component (true)
+    // } else {
+    //   //call callback from the index component (false)
+    // }
+    console.log(userAnswer + props.correctAnswer);
   }
 
   return(
     <div>
-    <div className="ui two buttons hSpace">
-      <button onClick={onAnswer}
-              value={props.answerList[0]}
-              className="ui basic green button">{props.answerList[0]}</button>
-      <button onClick={onAnswer}
-              value={props.answerList[1]}
-              className="ui basic green button">{props.answerList[1]}</button>
-    </div>
-    <div className="ui two buttons hSpace">
-      <button onClick={onAnswer}
-              value={props.answerList[2]}
-              className="ui basic green button">{props.answerList[2]}</button>
-      <button onClick={onAnswer}
-              value={props.answerList[3]}
-              className="ui basic green button">{props.answerList[3]}</button>
-    </div>
+      <div className="ui two buttons hSpace">{renderAnswers()}</div>
+      <div className="ui two buttons hSpace">{renderAnswers()}</div>
     </div>
   );
 
